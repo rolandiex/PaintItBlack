873e541cba2d886e72fe352c10ba177458416bcd
norm <norman.ziebal@web.de>
2019-08-17 01:57:14 +0200

    Updated UIWindowMgr script

--- scripts/UIWindowMgr.java
+++ scripts/UIWindowMgr.java
@@ -1,14 +1,18 @@
-//TODO write a description for this script
-//@author 
+//This script searches UIWindowMgr related functions and addresses.
+//- UIBookWnd__SendMsg
+//- UIWindowMgr__MakeWindow
+//It also generates the <client_date>.h file for Paint it Black
+//@author Norman Ziebal
 //@category pib
 //@keybinding 
 //@menupath 
 //@toolbar 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
 import java.util.Set;
+
+import javax.swing.JOptionPane;
+
 import java.nio.charset.*;
 
 import ghidra.app.script.GhidraScript;
@@ -26,23 +30,28 @@ import ghidra.program.model.pcode.*;
 import ghidra.program.model.address.*;
 
 public class UIWindowMgr extends GhidraScript {
-	final int INSTR_COUNT = 9;
-	final boolean DEBUG = true;
+	final int INSTR_COUNT = 9;		
+	final boolean DEBUG = false;	// Enables/disables debug prints.
+	boolean DRY_RUN = true;			// Does not apply changes to the ghidra database.
 	PiBLib pib;
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_CYAN = "\u001B[36m";
 
+	/* simple hex to 'signature' */
 	public String toSignature(String signature) {
 		return "\\x" + signature.replace(" ", "\\x");
 	}
 	
+	/* simple debug print */
 	public void printdbg(String str) {
 		if (DEBUG)
 			println(str);
 	}
-
-    public void run() throws Exception {
-    	pib = new PiBLib(false, "none", this);
-		FunctionManager fm = this.getCurrentProgram().getFunctionManager();
-		
+	
+	public int checkDate() throws MemoryAccessException {
 		/* Check date */
 		String ragexe = toSignature("44 3a 5c 53 6f 75 72 63 65 5c 4b 6f 72 65 61 5c 52 4f 5f");
 		printdbg("Looking for: " + ragexe);
@@ -50,7 +59,7 @@ public class UIWindowMgr extends GhidraScript {
 		Address ragexe_addr = findBytes(null, ragexe);
 		if(ragexe_addr == null) {
 			printerr("Unable to find client date!");
-			return;
+			return -1;
 		}
 		
 		printdbg("Client date: " + ragexe_addr);
@@ -83,10 +92,68 @@ public class UIWindowMgr extends GhidraScript {
 		
 		if (client_date_str == null) {
 			printerr("Failed to get Client date!");
-			return;
+			return -1;
 		}
 		
-		int client_date = Integer.parseInt(client_date_str);
+		var client_date_split = client_date_str.split("#");
+		return Integer.parseInt(client_date_split[0]);
+		
+	}
+	
+	public void run() throws Exception {
+		String[] args = getScriptArgs();
+		boolean TEST_RUN = false;
+		boolean HEADLESS = false;
+		
+		/* parsing command line args */
+		for (var arg : args) {
+			if (arg.equals("headless"))
+				HEADLESS = true;
+
+			if (arg.equals("dry-run"))
+				DRY_RUN = true;
+
+			if (arg.equals("test"))
+				TEST_RUN = true;
+		}	
+		
+		/* running headless */
+		if (HEADLESS) {
+			if (TEST_RUN) {
+				run_test();
+				return;
+			}
+			print(ANSI_CYAN + execute() + ANSI_RESET);
+			return;
+		}
+
+		/* running tests with gui */
+	    int dialogButton = JOptionPane.YES_NO_OPTION;
+	    int dialogResult = JOptionPane.showConfirmDialog (null, "Run tests?","Warning",dialogButton);
+	    if(dialogResult == JOptionPane.YES_OPTION){
+			run_test();
+	    	return;
+	    }
+	    
+	    /* running normal mode with gui */
+		dialogResult = JOptionPane.showConfirmDialog (null, "Dry run?","Warning",dialogButton);
+		if(dialogResult == JOptionPane.NO_OPTION){
+		  DRY_RUN = false;
+		}
+		print(execute());
+	}
+
+    public String execute() throws Exception {
+    	pib = new PiBLib(false, "none", this);
+		FunctionManager fm = this.getCurrentProgram().getFunctionManager();
+		
+		String output = "\n";
+		
+		int client_date = checkDate();
+		if (client_date == -1) {
+			printerr("Client date was not found!");
+			return "";
+		}
     	
     	//////////////////////////////////////////////////////////////////////////////////////////////////////////
     	// Searching for UIWindowMgr__MakeWindow
@@ -97,63 +164,83 @@ public class UIWindowMgr extends GhidraScript {
 		Address sig_addr = findBytes(null, sig);
 		if (sig_addr == null) {
 			printerr("UIWindowMgr_MakeWindow: signatur not found!");
-			return;
+			return "";
 		}
 		
 		printdbg("Signature found at: " + sig_addr.toString());
 		
 		var instr = getInstructionAt(sig_addr);
-		for (int i = 0; i < INSTR_COUNT; i++)
+		for (int i = 0; i < INSTR_COUNT; i++) // Its the 9. instruction after the signature address
 			instr = instr.getNext();
 		
 		printdbg("UIWindowMgr__MakeWindow: " + instr.toString());
 		
 		/* header */
-		print("//\n");
-		print("// CUIWindowMgr defines\n");
-		print("//\n");
+		output += "//\n";
+		output += "// CUIWindowMgr defines\n";
+		output += "//\n";
 		
 		/* parsed address for UIWindowMgr__MakeWindow */
 		Address addr = parseAddress(instr.toString().replace("CALL ", ""));
-		print("#define UIWINDOWMGR_MAKEWINDOW_FN 0x" + addr + "\n");
+		output += "#define UIWINDOWMGR_MAKEWINDOW_FN 0x" + addr + "\n";
+		
+		/* get UIWindowMgr_PTR */
+		printdbg("Searching UIWindowMgr_PTR. Start search at: " + instr.getAddress().toString());
+		Address tmp = null;
+		for (int i = 0; i < 5; /* only check the previous 5 instructions */ i++) {
+			 tmp = instr.getPrevious().getAddress(); // instr: CALL UIWindowMgr_MakeWindow
+			 if (getByte(tmp) == 0xb9) { // looking for mov ecx
+				 printdbg("UIWindowMgr_PTR: MOV ECX found!");
+				 break;
+			 }
+		}
+		
+		var mov_ecx = getInstructionAt(tmp); // this should be MOV ECX, UIWINDOWMGR_PTR now!
+		printdbg("UIWindowMgr_PTR: " + mov_ecx);
+		var UIWindowMgr_PTR = parseAddress(mov_ecx.toString().replace("MOV ECX,", ""));
+		printdbg("UIWindowMgr_PTR address: " + UIWindowMgr_PTR);
+		output += "#define UIWINDOWMGR_PTR 0x" + UIWindowMgr_PTR.toString();
 		
 		/* set function parameters */
-		if (client_date >= 20150513) {
-			printdbg("Config function UIWINDOWMGR_MAKEWINDOW_FN: " + addr);
-			var UIWindowMgr_MakeWindow_fn = fm.getFunctionContaining(addr);
-			UIWindowMgr_MakeWindow_fn.setName("UIWindowMgr::MakeWindow", SourceType.USER_DEFINED);
-			
-			var this_ptr = 	new ParameterImpl("this", new Pointer32DataType(), this.getCurrentProgram());
-			var a1 = 		new ParameterImpl("a1", new IntegerDataType(), this.getCurrentProgram());
-			var ret_var = 	new ReturnParameterImpl(new Pointer32DataType(), this.getCurrentProgram());
-			
-			UIWindowMgr_MakeWindow_fn.updateFunction("__thiscall", ret_var, 
-					FunctionUpdateType.DYNAMIC_STORAGE_ALL_PARAMS, false, 
-					SourceType.USER_DEFINED, this_ptr, a1);
-		} else {
-			printerr("Unable to set function parameters for UIWindowMgr::MakeWindow: Clientdate: " + client_date);
-			return;
+		printdbg("Config function UIWINDOWMGR_MAKEWINDOW_FN: " + addr);
+		var UIWindowMgr_MakeWindow_fn = fm.getFunctionContaining(addr);
+		UIWindowMgr_MakeWindow_fn.setName("UIWindowMgr::MakeWindow", SourceType.USER_DEFINED);
+		
+		if (!DRY_RUN) {
+			if (client_date >= 20150513) {	
+				var this_ptr = 	new ParameterImpl("this", new Pointer32DataType(), this.getCurrentProgram());
+				var a1 = 		new ParameterImpl("a1", new IntegerDataType(), this.getCurrentProgram());
+				var ret_var = 	new ReturnParameterImpl(new Pointer32DataType(), this.getCurrentProgram());
+				
+				UIWindowMgr_MakeWindow_fn.updateFunction("__thiscall", ret_var, 
+						FunctionUpdateType.DYNAMIC_STORAGE_ALL_PARAMS, false, 
+						SourceType.USER_DEFINED, this_ptr, a1);
+			} else {
+				printerr("Unable to set function parameters for UIWindowMgr::MakeWindow: Clientdate: " + client_date);
+				return "";
+			}
 		}
 		
 		/* add static content */
-		print("\n");
-		print("struct UIFrameWnd {};\n");
-		print("typedef UIFrameWnd* (__thiscall* lpMakeWindow)(void*, int);\n");
-		print("\n");
+		output += "\n";
+		output += "struct UIFrameWnd {};\n";
+		output += "typedef UIFrameWnd* (__thiscall* lpMakeWindow)(void*, int);\n";
+		output += "\n";
 		
     	//////////////////////////////////////////////////////////////////////////////////////////////////////////
     	// Searching for UIBookWnd__SendMsg
     	//
 		
 		/* add static content */
-		print("\t// sub types\n");
-		print("\t// CUIBookWnd\n");
+		output += "\t// sub types\n";
+		output += "\t// CUIBookWnd\n";
 		
 		
 		/* search function */
 		Set<Function> func_set = pib.string_to_reflist("%sbook\\%s.txt");
 		
 		Function func_addr = null;
+		Function func_addr_tmp = null;
 		int counter = 0;
 		sig = toSignature("6a 00 6a 00 6a 00 68.{0,4} 6a 06");
 		for(Iterator<Function> it = func_set.iterator(); it.hasNext();) {
@@ -165,53 +252,124 @@ public class UIWindowMgr extends GhidraScript {
 			
 			var sig_addr_lst = findBytes(func_addr.getBody(), sig, 1, 2, false);
 			if (sig_addr_lst.length > 0) {
+				printdbg("Func_addr: " + func_addr);
 				printdbg("Signature found at: " + sig_addr_lst[0]);
+				func_addr_tmp = func_addr;
 				counter++;
 			}
 		}
 		
 		if (counter > 1) {
 			printerr("UIWindowMgr: counter was greater 1");
-			return;
+			return "";
 		}
+		
+		func_addr = func_addr_tmp ;
 			
 		printdbg("UIBookWnd__SendMsg: " + func_addr.getEntryPoint());
-		print("\t#define UIBOOKWND_SENDMSG_FN 0x" + func_addr.getEntryPoint() + "\n");
+		output += "\t#define UIBOOKWND_SENDMSG_FN 0x" + func_addr.getEntryPoint() + "\n";
 		
 		/* set function parameters */
-		if (client_date >= 20150513) {
-			var UIBookWnd_SendMsg_fn = fm.getFunctionContaining(func_addr.getEntryPoint());
-			UIBookWnd_SendMsg_fn.setName("UIBookWnd::SendMsg", SourceType.USER_DEFINED);
-			
-			var this_ptr = 	new ParameterImpl("this", new Pointer32DataType(), this.getCurrentProgram());
-			var a1 = 		new ParameterImpl("a1", new IntegerDataType(), this.getCurrentProgram());
-			var a2 =		new ParameterImpl("a2", new IntegerDataType(), this.getCurrentProgram());
-			var a3 =		new ParameterImpl("a3", new Pointer8DataType(), this.getCurrentProgram());
-			var a4 = 		new ParameterImpl("a4", new IntegerDataType(), this.getCurrentProgram());
-			var a5 =		new ParameterImpl("a5", new IntegerDataType(), this.getCurrentProgram());
-			var a6 =		new ParameterImpl("a6", new IntegerDataType(), this.getCurrentProgram());
-			var ret_var = 	new ReturnParameterImpl(new VoidDataType(), this.getCurrentProgram());
-			
-			UIBookWnd_SendMsg_fn.updateFunction("__thiscall", ret_var, 
-					FunctionUpdateType.DYNAMIC_STORAGE_ALL_PARAMS, false, 
-					SourceType.USER_DEFINED, 
-					this_ptr, a1, a2, a3, a4, a5, a6);
-		} else {
-			printerr("Unable to set function parameters for UIWindowMgr::MakeWindow: Clientdate: " + client_date);
-			return;
+		var UIBookWnd_SendMsg_fn = fm.getFunctionContaining(func_addr.getEntryPoint());
+		UIBookWnd_SendMsg_fn.setName("UIBookWnd::SendMsg", SourceType.USER_DEFINED);
+		
+		if (!DRY_RUN) {
+			if (client_date >= 20150513) {
+				var this_ptr = 	new ParameterImpl("this", new Pointer32DataType(), this.getCurrentProgram());
+				var a1 = 		new ParameterImpl("a1", new IntegerDataType(), this.getCurrentProgram());
+				var a2 =		new ParameterImpl("a2", new IntegerDataType(), this.getCurrentProgram());
+				var a3 =		new ParameterImpl("a3", new Pointer8DataType(), this.getCurrentProgram());
+				var a4 = 		new ParameterImpl("a4", new IntegerDataType(), this.getCurrentProgram());
+				var a5 =		new ParameterImpl("a5", new IntegerDataType(), this.getCurrentProgram());
+				var a6 =		new ParameterImpl("a6", new IntegerDataType(), this.getCurrentProgram());
+				var ret_var = 	new ReturnParameterImpl(new VoidDataType(), this.getCurrentProgram());
+				
+				UIBookWnd_SendMsg_fn.updateFunction("__thiscall", ret_var, 
+						FunctionUpdateType.DYNAMIC_STORAGE_ALL_PARAMS, false, 
+						SourceType.USER_DEFINED, 
+						this_ptr, a1, a2, a3, a4, a5, a6);
+			} else {
+				printerr("Unable to set function parameters for UIWindowMgr::MakeWindow: Clientdate: " + client_date);
+				return "";
+			}
 		}
 		
 		/* add static content */
-		print("\n");
-		print("\ttypedef void(__thiscall* lpSendMsg)(void*, int, int, char*, int, int, int);\n");
-		print("\t//struct CUIBookWnd {\n");
-		print("\t#define UIBOOKWND_DATA		\\\n");
+		output += "\n";
+		output += "\ttypedef void(__thiscall* lpSendMsg)(void*, int, int, char*, int, int, int);\n";
+		output += "\t//struct CUIBookWnd {\n";
+		output += "\t#define UIBOOKWND_DATA		\\\n";
 		
 		//
 		// Searching for book_title location
 		//
-		print("\t\tTODO\n");
-		print("\t//}\n");
+		sig = toSignature("8d 86.{4} 50 8b ce e8.{4}");
+		var tmp_sig_addr = findBytes(UIBookWnd_SendMsg_fn.getBody(), sig, 1, 1, false);
+		if (tmp_sig_addr == null || tmp_sig_addr.length != 2) {
+			if (tmp_sig_addr != null)
+				printdbg("Size: " + tmp_sig_addr.length);
+			for (var f : tmp_sig_addr)
+				printdbg(f.toString());
+			printerr("Book Title signature: Error!");
+			return "";
+		}
+		printdbg("BookTitle signature: " + tmp_sig_addr[0]);
 		
+		var offset_tmp = getByte(tmp_sig_addr[0].add(2)); // get the first signature
+		var book_title_offset = String.format("%02x", offset_tmp);
+		printdbg("BookTitle offset: " + book_title_offset);
+		
+		output += "\t\t/* 0x0	*/ BYTE offset0[0x" + book_title_offset + "];	\\\n";
+		output += "\t\t/* 0x" + book_title_offset +"	*/ char book_title[64];\n";
+		output += "\t//}\n";
+		
+		return output;
    	}
+
+	public void run_test() throws Exception {
+		DRY_RUN = true; // test are always dry runs!
+		if (!test(execute()))
+	    	printerr(ANSI_RED + "Test failed!" + ANSI_RESET);
+	    else
+	    	println(ANSI_GREEN + "Test successful!" + ANSI_RESET);
+	}
+    
+    /* used to test correct output.
+     * is based on already known and confirmed outputs.
+     */
+    public boolean test(String output) throws MemoryAccessException {
+    	int client_date = checkDate();
+    	
+    	/* Test 20150513 client output */
+    	if (client_date == 20150513) {
+    		String test_str = "\n//\n" + 
+    				"// CUIWindowMgr defines\n" + 
+    				"//\n" + 
+    				"#define UIWINDOWMGR_MAKEWINDOW_FN 0x005e95a0\n" + 
+    				"#define UIWINDOWMGR_PTR 0x00c6f600\n" + 
+    				"struct UIFrameWnd {};\n" + 
+    				"typedef UIFrameWnd* (__thiscall* lpMakeWindow)(void*, int);\n" + 
+    				"\n" + 
+    				"	// sub types\n" + 
+    				"	// CUIBookWnd\n" + 
+    				"	#define UIBOOKWND_SENDMSG_FN 0x005199f0\n" + 
+    				"\n" + 
+    				"	typedef void(__thiscall* lpSendMsg)(void*, int, int, char*, int, int, int);\n" + 
+    				"	//struct CUIBookWnd {\n" + 
+    				"	#define UIBOOKWND_DATA		\\\n" + 
+    				"		/* 0x0	*/ BYTE offset0[0x84];	\\\n" + 
+    				"		/* 0x84	*/ char book_title[64];\n" + 
+    				"	//}\n";
+    		
+    		boolean res = output.equals(test_str);
+    		if (res)
+    			return true;
+			print(test_str);
+			print(output);
+			return false;
+    	}
+    	
+    	printerr("No valid client date for test!");
+    	return false;
+    }
 }
